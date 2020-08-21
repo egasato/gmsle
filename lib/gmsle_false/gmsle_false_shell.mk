6db13a48e5906d08b957ab3524d2adb780f185c1
egasato <2669347+egasato@users.noreply.github.com>
2020-08-21 18:46:54 +0200

    Add 'gmsle_false' definition

---
+++ lib/gmsle_false/gmsle_false_shell.mk
@@ -0,0 +1,7 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+## POSIX Shell Implementation
+
+## A constant that represents a logical/boolean 'false'.
+gmsle_false_shell := $(shell $(GMSLE_ECHO))
