9c75778f1f2a834c8d34f2a4f57e4f602a529caf
egasato <2669347+egasato@users.noreply.github.com>
2020-08-21 18:33:17 +0200

    Add 'GMSLE_PREFER_*' enum values

--- enums.mk
+++ enums.mk
@@ -12,3 +12,11 @@ GMSLE_WARNINGS_YES := 2
 # Represents the error policy
 GMSLE_ERRORS_NO  := 1
 GMSLE_ERRORS_YES := 2
+
+# Represents the implementation preference policy
+GMSLE_PREFER_MAKE  := 1
+GMSLE_PREFER_GUILE := 2
+GMSLE_PREFER_SHELL := 3
+GMSLE_PREFER_AWK   := 4
+GMSLE_PREFER_SED   := 5
+GMSLE_PREFER_GREP  := 6
