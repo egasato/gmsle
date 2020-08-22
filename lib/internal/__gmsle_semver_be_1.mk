ddb6ccf59adfdd72f5949aff953a4b31488e1607
egasato <2669347+egasato@users.noreply.github.com>
2020-08-22 23:05:43 +0200

    Add 'strip' support check

---
+++ lib/internal/__gmsle_semver_be_1.mk
@@ -0,0 +1,13 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Include guard to avoid double inclusion
+ifndef __gmsle_semver_be_1
+
+    # Make sure a minimum version is being used
+    ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+        include $(LIB_GMSLE_PATH)/lib/gmsle_semver/GNUmakefile
+    endif
+    __gmsle_semver_be_1 := $(call gmsle_semver,$(MAKE_VERSION),>=,1)
+
+endif #__gmsle_semver_be_1
