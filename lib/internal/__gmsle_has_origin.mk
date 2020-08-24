cb32b8e787a04ebc83d29d532308c5c63bf437b9
egasato <2669347+egasato@users.noreply.github.com>
2020-08-24 19:33:17 +0200

    Add 'origin' support check

---
+++ lib/internal/__gmsle_has_origin.mk
@@ -0,0 +1,33 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Include guard to avoid double inclusion
+ifndef __gmsle_has_origin
+
+    # Make sure a minimum version is being used
+    ifndef __gmsle_include_ifndef
+        include $(LIB_GMSLE_PATH)/lib/internal/__gmsle_include_ifndef.mk
+    endif
+    $(call __gmsle_include_ifndef,__gmsle_semver_be_3_03,$(LIB_GMSLE_PATH)/lib/internal/__gmsle_semver_be_3_03.mk)
+
+    ifneq '$(__gmsle_semver_be_3_03)' ''
+        __gmsle_has_origin := $(origin __gmsle_has_origin)
+        ifeq '$(__gmsle_has_origin)' 'undefined'
+            ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+                $(call __gmsle_include_ifndef,gmsle_true,$(LIB_GMSLE_PATH)/lib/gmsle_true/GNUmakefile)
+            endif
+            __gmsle_has_origin := $(gmsle_true)
+        else
+            ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+                $(call __gmsle_include_ifndef,gmsle_false,$(LIB_GMSLE_PATH)/lib/gmsle_false/GNUmakefile)
+            endif
+            __gmsle_has_origin := $(gmsle_false)
+        endif
+    else
+        ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+            $(call __gmsle_include_ifndef,gmsle_false,$(LIB_GMSLE_PATH)/lib/gmsle_false/GNUmakefile)
+        endif
+        __gmsle_has_origin := $(gmsle_false)
+    endif
+
+endif #__gmsle_has_origin
