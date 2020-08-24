68faa00038abf9d15e09d31c882a1c4986d5e805
egasato <2669347+egasato@users.noreply.github.com>
2020-08-24 19:33:11 +0200

    Add 'notdir' support check

---
+++ lib/internal/__gmsle_has_notdir.mk
@@ -0,0 +1,33 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Include guard to avoid double inclusion
+ifndef __gmsle_has_notdir
+
+    # Make sure a minimum version is being used
+    ifndef __gmsle_include_ifndef
+        include $(LIB_GMSLE_PATH)/lib/internal/__gmsle_include_ifndef.mk
+    endif
+    $(call __gmsle_include_ifndef,__gmsle_semver_be_1,$(LIB_GMSLE_PATH)/lib/internal/__gmsle_semver_be_1.mk)
+
+    ifneq '$(__gmsle_semver_be_1)' ''
+        __gmsle_has_notdir := $(notdir src/foo.c hacks)
+        ifeq '$(__gmsle_has_notdir)' 'foo.c hacks'
+            ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+                $(call __gmsle_include_ifndef,gmsle_true,$(LIB_GMSLE_PATH)/lib/gmsle_true/GNUmakefile)
+            endif
+            __gmsle_has_notdir := $(gmsle_true)
+        else
+            ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+                $(call __gmsle_include_ifndef,gmsle_false,$(LIB_GMSLE_PATH)/lib/gmsle_false/GNUmakefile)
+            endif
+            __gmsle_has_notdir := $(gmsle_false)
+        endif
+    else
+        ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+            $(call __gmsle_include_ifndef,gmsle_false,$(LIB_GMSLE_PATH)/lib/gmsle_false/GNUmakefile)
+        endif
+        __gmsle_has_notdir := $(gmsle_false)
+    endif
+
+endif #__gmsle_has_notdir
