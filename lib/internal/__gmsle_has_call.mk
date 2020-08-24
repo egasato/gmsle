382f77f71cb98b4c846b656a533be932b994ee1d
egasato <2669347+egasato@users.noreply.github.com>
2020-08-24 19:32:21 +0200

    Add 'call' support check

---
+++ lib/internal/__gmsle_has_call.mk
@@ -0,0 +1,35 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+# Include guard to avoid double inclusion
+ifndef __gmsle_has_call
+
+    # Make sure a minimum version is being used
+    ifndef __gmsle_include_ifndef
+        include $(LIB_GMSLE_PATH)/lib/internal/__gmsle_include_ifndef.mk
+    endif
+    $(call __gmsle_include_ifndef,__gmsle_semver_be_3_77_90,$(LIB_GMSLE_PATH)/lib/internal/__gmsle_semver_be_3_77_90.mk)
+
+    ifneq '$(__gmsle_semver_be_3_77_90)' ''
+        ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+            $(call __gmsle_include_ifndef,gmsle_true,$(LIB_GMSLE_PATH)/lib/gmsle_true/GNUmakefile)
+        endif
+        __gmsle_has_call = $(1)
+        __gmsle_has_call := $(call __gmsle_has_call,$(gmsle_true))
+        ifeq '$(__gmsle_has_call)' '$(gmsle_true)'
+            __gmsle_has_call = $(gmsle_true)
+        else
+            ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+                $(call __gmsle_include_ifndef,gmsle_false,$(LIB_GMSLE_PATH)/lib/gmsle_false/GNUmakefile)
+            endif
+            __gmsle_has_call = $(gmsle_false)
+        endif
+    else
+        ifeq '$(GMSLE_INCLUDE)' '$(GMSLE_INCLUDE_AUTOMATIC)'
+            $(call __gmsle_include_ifndef,gmsle_false,$(LIB_GMSLE_PATH)/lib/gmsle_false/GNUmakefile)
+        endif
+        __gmsle_has_call = $(gmsle_false)
+    endif
+
+endif #__gmsle_has_call
