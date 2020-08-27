4af0da61c81b7ac59adbb9951f206a266ee412f9
egasato <2669347+egasato@users.noreply.github.com>
2020-08-27 22:52:59 +0200

    Add 'gmsle_findstring' test

---
+++ test/lib/gmsle_findstring/gmsle_findstring.mk
@@ -0,0 +1,19 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Load the dependencies
+include $(LIB_GMSLE_PATH)/lib/$(test_name)/$(test_name)_$(test_type).mk
+
+# Test the variable
+test_in1 := $(shell echo '	a ')
+test_in2 := $(shell echo '	a b c	')
+test_exp := $(test_in1)
+test_out := $(call $(test_name)_$(test_type),$(test_in1),$(test_in2))
+
+# Print the result
+ifneq '$(test_out)' '$(test_exp)'
+    $(warning The implementation of '$(test_name)_$(test_type)' is wrong.)
+    $(info Input:    '$(test_in1)', '$(test_in2)')
+    $(info Expected: '$(test_exp)')
+    $(info Output:   '$(test_out)')
+endif
