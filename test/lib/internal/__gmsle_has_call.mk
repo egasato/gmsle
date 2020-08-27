3973e2293228d067723bab9e4608317f4bd4c833
egasato <2669347+egasato@users.noreply.github.com>
2020-08-27 13:30:13 +0200

    Add 'call' test

---
+++ test/lib/internal/__gmsle_has_call.mk
@@ -0,0 +1,14 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Set some useful variables
+test_name = call
+test_var  = __gmsle_has_$(test_name)
+
+# Include the library check
+include $(LIB_GMSLE_PATH)/lib/internal/__gmsle_has_$(test_name).mk
+
+# Print a warning if not supported
+ifeq '$($(test_var))' ''
+    $(info The feature '$(test_name)' is not available, as defined by '$(test_var)'.)
+endif
