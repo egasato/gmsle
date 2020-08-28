e405ae69078af8fbfe2dca2fb65082122a3202d9
egasato <2669347+egasato@users.noreply.github.com>
2020-08-28 14:23:11 +0200

    Add 'gmsle_notdir' test

---
+++ test/lib/gmsle_notdir/gmsle_notdir.mk
@@ -0,0 +1,18 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Load the dependencies
+include $(LIB_GMSLE_PATH)/lib/$(test_name)/$(test_name)_$(test_type).mk
+
+# Test the variable
+test_in1 := $(shell echo '	src/foo.c hacks	')
+test_exp := foo.c hacks
+test_out := $(call $(test_name)_$(test_type),$(test_in1))
+
+# Print the result
+ifneq '$(test_out)' '$(test_exp)'
+    $(warning The implementation of '$(test_name)_$(test_type)' is wrong.)
+    $(info Input:    '$(test_in1)')
+    $(info Expected: '$(test_exp)')
+    $(info Output:   '$(test_out)')
+endif
