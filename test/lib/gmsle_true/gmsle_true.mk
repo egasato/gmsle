bf50522f21dc9ee730689c2071e0f2141eb02c4f
egasato <2669347+egasato@users.noreply.github.com>
2020-08-28 15:24:17 +0200

    Add 'gmsle_true' test

---
+++ test/lib/gmsle_true/gmsle_true.mk
@@ -0,0 +1,16 @@
+#!/usr/bin/env make -f
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Load the dependencies
+include $(LIB_GMSLE_PATH)/lib/$(test_name)/$(test_name)_$(test_type).mk
+
+# Test the variable
+test_out := $($(test_name)_$(test_type))
+
+# Print the result
+ifeq '$(test_out)' ''
+    $(warning The implementation of '$(test_name)_$(test_type)' is wrong.)
+    $(info Input:    '$(test_in1)')
+    $(info Expected: ! '')
+    $(info Output:   '$(test_out)')
+endif
